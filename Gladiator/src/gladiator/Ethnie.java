bc42025642090eaf38785a5b60d02ea8ef5fd070
RAPPENECKER Bastien <brappenecker@troglo.iutrs.unistra.fr>
2017-05-17 12:07:39 +0200

    Ajout fichiers initiaux

---
+++ Gladiator/src/gladiator/Ethnie.java
@@ -0,0 +1,18 @@
+package gladiator;
+
+import java.util.Collection;
+
+public class Ethnie {
+    /**
+     * @attribute
+     */
+    private Integer ide;
+
+    /**
+     * @attribute
+     */
+    private String nom;
+
+    public static Collection listerGladiateur() {
+    }
+}
